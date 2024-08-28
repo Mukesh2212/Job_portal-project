90b5f5d1a245ac4e5f8b57b1b39fd20c0e8c54b3
mukesh7260 <mk2648054@gmail.com>
2024-08-28 16:02:56 +0530

    second commit

--- employee_dashboard/urls.py
+++ employee_dashboard/urls.py
@@ -9,10 +9,10 @@ from django.conf.urls.static import static
 urlpatterns = [
    
     path("employee/", views.EmployeeApiView.as_view(),name="employee"),
-    path("employeeimg/", views.EmployeeApiViewImg.as_view(),name="employee"),
-    path("feedback-employer/", views.EmployerApiView.as_view(),name="employee"),
-    path("whatapps-support-api/", views.EmployerApiView.as_view(),name="employee"),
-    path("employer-schedule/", views.EmployerScheduleApiView.as_view(),name="employee"),
+    path("employeeimg/", views.EmployeeApiViewImg.as_view(),name="employeeimg"),
+    path("feedback-employer/", views.EmployerApiView.as_view(),name="feedback-employer"),
+    path("whatapps-support-api/", views.EmployerApiView.as_view(),name="whatapps-support-api"),
+    path("employer-schedule/", views.EmployerScheduleApiView.as_view(),name="employer-schedule"),
 
 
 ]
